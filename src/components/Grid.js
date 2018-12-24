75ad97766bbe64d5da769d8e58b474d83e8fe7f1
Ryan Santos <ryansantos86@gmail.com>
2018-12-23 20:45:18 -0800

    board logic

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,23 +2,41 @@ import React, { Component } from 'react';
 import Cell from './Cell';
 
 export default class Grid extends Component {
-  renderCells = () => {
+  constructor() {
+    super();
+    this.state = {
+      player: 1,
+      gameOver: false,
+      winner: false,
+      board: this.createBoard()
+    };
+  }
+
+  nextPlayer = () => {};
+
+  createBoard = () => {
     const board = [];
     for (let i = 0; i < 3; i++) {
       const row = [];
       for (let j = 0; j < 3; j++) {
-        row.push(<Cell key={`${i}-${j}`} cell={`${i}-${j}`} />);
+        row.push(0);
       }
       board.push(row);
     }
     return board;
   };
 
+  renderCells = () => {
+    return this.state.board.map((row, x) => {
+      return row.map((_, y) => <Cell key={`${x}-${y}`} cell={`${x}-${y}`} />);
+    });
+  };
+
   render() {
     return (
       <div className="grid-container">
-        <div />
         <div className="grid">{this.renderCells()}</div>
+        <button onClick={() => console.log(this.state)}>test</button>
       </div>
     );
   }
