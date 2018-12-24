494a8154e19a2ae6f9a4eeb7902779c936b9dbed
Ryan Santos <ryansantos86@gmail.com>
2018-12-24 12:57:20 -0800

    commit

--- src/Cell.js
+++ src/Cell.js
@@ -8,21 +8,20 @@ export default class Cell extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      clicked: false,
-      cell: this.props.cell
+      clicked: false
     };
   }
 
   handleClick = cell => {
-    this.refs[this.state.cell].setAttribute('disabled', 'disabled');
+    this.refs[this.props.cell].setAttribute('disabled', 'disabled');
     this.setState({ clicked: true }, () => {
-      const coords = cell.split('-').map(num => parseInt(num));
+      const coords = parseIntCoords(cell);
       this.props.handleClick(coords);
     });
   };
 
   renderMove = () => {
-    const [x, y] = parseIntCoords(this.state.cell);
+    const [x, y] = parseIntCoords(this.props.cell);
     const { getCell } = this.props.board;
     return getCell(x, y) === 1 ? 'x' : getCell(x, y) === 2 ? 'o' : '';
   };
@@ -33,7 +32,7 @@ export default class Cell extends Component {
       <button
         className={`cell cell-${cell}`}
         onClick={() => this.handleClick(cell)}
-        ref={this.state.cell}
+        ref={this.props.cell}
       >
         {this.renderMove()}
       </button>
