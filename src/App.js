1116cf38894b263190ed2cab9c148022bb722960
Ryan Santos <ryansantos86@gmail.com>
2018-12-26 16:56:28 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,113 @@
 import React, { Component } from 'react';
-import Grid from './Grid';
+import Board from './Board';
+import Cell from './Cell';
 import './App.scss';
 
-const Title = () => {
-  return (
-    <div className="title-container">
-      <h2>Tic Tac Toe!</h2>
-    </div>
-  );
-};
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      player: 1,
+      gameOver: false
+    };
+    this.board = new Board();
+  }
 
-class App extends Component {
-  componentDidMount() {
-    document.title = 'Tic Tac Toe!';
+  // check winner
+  checkWinner = () => {
+    if (this.board.findWinner() > 0) this.setState({ gameOver: true });
+  };
+
+  // switch players
+  nextPlayer = () => {
+    return this.state.player === 1 ? 2 : 1;
+  };
+
+  // set coordinates on board to player making move
+  handleClick = cell => {
+    const [x, y] = cell;
+    !this.state.gameOver &&
+      this.board.movePlayer(x, y, this.state.player, this.playerMove);
+  };
+
+  // player move then ai move
+  playerMove = () => {
+    this.checkWinner();
+    this.setState({ player: this.nextPlayer() }, () => {
+      setTimeout(this.aiMove, 200); // realistic time between player and ai move
+    });
+  };
+
+  // ai move to random playable cell
+  aiMove = () => {
+    const openCells = this.board.getOpenCells();
+    if (openCells.length) {
+      const [x, y] = openCells[Math.floor(Math.random() * openCells.length)];
+      !this.state.gameOver &&
+        this.board.movePlayer(x, y, this.state.player, () => {
+          this.forceUpdate();
+          this.checkWinner();
+        });
+      this.setState({ player: this.nextPlayer() });
+    }
+  };
+
+  addClassName = (x, y) => {
+    const { getCell } = this.board;
+    return getCell(x, y) === 1 ? 'player1' : getCell(x, y) === 2 ? 'player2' : '';
+  };
+
+  // show x or o based on who's turn it is
+  renderMove = (x, y) => {
+    return <div className={this.addClassName(x, y)} />;
+  };
+
+  // show cells based on board layout
+  renderGrid = () => {
+    return this.board.board.map((row, x) => {
+      return row.map((cell, y) => {
+        const coords = `${x}-${y}`;
+        const disabled = cell > 0; // disable clicking if move exists on cell
+        return (
+          <Cell
+            key={coords}
+            cell={coords}
+            handleClick={this.handleClick}
+            disabled={disabled}
+          >
+            {this.renderMove(x, y)}
+          </Cell>
+        );
+      });
+    });
+  };
+
+
+  renderAnnouncement = () => {
+    const announceWinner = (
+      <div className="backdrop">
+        <div className="modal-wrapper">
+           player {this.state.player} has won! <button onClick={this.reset}>reset</button>
+        </div>
+      </div>
+    );
+    return this.state.gameOver ? announceWinner : '';
+  };
+
+  reset = () => {
+    this.board = new Board();
+    this.setState({player: 1, gameOver: false})
   }
 
   render = () => {
     return (
       <div className="app">
-        <Title />
-        <Grid />
+        {this.renderAnnouncement()}
+        <div className="title-container">
+          <h2>Tic Tac Toe!</h2>
+        </div>
+        <div className="grid">{this.renderGrid()}</div>;
       </div>
     );
   };
 }
-
-export default App;
--- src/Board.js
+++ src/Board.js
@@ -33,9 +33,45 @@ export default class Board {
     }, []);
   };
 
-  // TODO: check rows, columns, diagonals
-  checkWin = () => {
+  findWinner = () => {
     const { board } = this;
-    console.log(board);
+
+    // find column matches
+    const columns = [];
+    for (let rowIdx = 0; rowIdx < board.length; rowIdx++) {
+      const col = [];
+      for (let colIdx = 0; colIdx < board[rowIdx].length; colIdx++) {
+        col.push(board[colIdx][rowIdx]);
+      }
+      columns.push(col);
+    }
+
+    // find diagonal matches
+    const diags = { left: [], right: [] };
+    for (let i = 0; i < board.length; i++) {
+      diags.left.push(board[i][i]);
+      diags.right.push(board[board.length - i - 1][i]);
+    }
+
+    // check if all items in a single row are the same
+    const allTheSame = row => {
+      return row.every(cell => cell === row[0]);
+    };
+
+    // get winner after row, column, and diagonal matches are aggregated into arrays
+    const checkMatrix = matrix => {
+      for (let x = 0; x < matrix.length; x++) {
+        if (allTheSame(matrix[x]) && matrix[x][0]) {
+          return matrix[x][0];
+        }
+      }
+    };
+
+    // check rows, columns, and diagonals and return player 1, player 2, or nothing
+    return checkMatrix(board)
+      ? checkMatrix(board)
+      : checkMatrix(columns)
+      ? checkMatrix(columns)
+      : checkMatrix([diags.left, diags.right]);
   };
 }
